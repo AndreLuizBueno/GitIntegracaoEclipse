29ed1dd26ca5d62d42ea6c7de6b40711e6d013c4
AndreLuizBueno <andreluizbueno.148@gmail.com>
2024-07-03 14:37:49 -0300

    Teste GitHUB - 03/07/2024

---
+++ IntegracaoGit/src/GitTeste/TesteGitEclipse.java
@@ -0,0 +1,10 @@
+package GitTeste;
+
+public class TesteGitEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integração Git Eclipse Primeiro Teste.");
+	}
+
+}
