8c19d718a10a3a2d5ae5049ffee256fce8542d0a
André Luiz Bueno <43247820+AndreLuizBueno@users.noreply.github.com>
2024-07-03 15:09:50 -0300

    Update TesteGitEclipse.java

--- IntegracaoGit/src/GitTeste/TesteGitEclipse.java
+++ IntegracaoGit/src/GitTeste/TesteGitEclipse.java
@@ -5,6 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Integração Git Eclipse Primeiro Teste.");
+		System.out.println("Teste Git Eclipse Vamo dale pra não tomale.");
 	}
 
 }
